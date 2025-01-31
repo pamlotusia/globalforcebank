509f7f31ee6e3b94246be9eec11f580ae92bdc65
Pamela Lima <pamela.jesus.lima@gmail.com>
2025-01-31 12:50:59 -0300

    correntista com trigger concluido

--- backend/src/controllers/UsersController.js
+++ backend/src/controllers/UsersController.js
@@ -1,45 +1,44 @@
-import { pool } from '../database/connection.js'
-import bcrypt from 'bcrypt'
-// import { BankAccount } from './BankAccountController.js'
+import { pool } from "../database/connection.js";
+import bcrypt from "bcrypt";
+
 export class UsersController {
   async create(req, res) {
-    const db = pool
-    const bankAccount = new BankAccount()
+    const db = pool;
     try {
-      const { name, email, cpf, password } = req.body
-      const hashedPassword = await bcrypt.hash(password, 8)
+      const { name, email, cpf, password } = req.body;
+      const hashedPassword = await bcrypt.hash(password, 8);
 
       const insert = `
       INSERT INTO correntistas (name, email, cpf, password)
       VALUES ($1, $2, $3, $4)
       RETURNING *
-      `
+      `;
 
-      const values = [name, email, cpf, hashedPassword]
-      // bankAccount.create()
-      const result = await db.query(insert, values)
+      const values = [name, email, cpf, hashedPassword];
+
+      const result = await db.query(insert, values);
 
       return res
         .status(201)
-        .json({ user: result.rows[0], message: 'Usuário criado com sucesso!' })
+        .json({ user: result.rows[0], message: "Usuário criado com sucesso!" });
     } catch (e) {
-      console.error(e)
-      return res.status(500).json({ message: 'erro ao criar correntistas' })
+      console.error(e);
+      return res.status(500).json({ message: "erro ao criar correntistas" });
     }
   }
 
   async index(req, res) {
     try {
-      const db = pool
+      const db = pool;
 
       const select = `
-      SELECT * FROM correntistas`
-      const result = await db.query(select)
+      SELECT * FROM correntistas`;
+      const result = await db.query(select);
 
-      return res.status(200).json(result.rows)
+      return res.status(200).json(result.rows);
     } catch (e) {
-      console.error(e)
-      return res.status(500).json({ message: 'erro ao listar correntistas' })
+      console.error(e);
+      return res.status(500).json({ message: "erro ao listar correntistas" });
     }
   }
 }
--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -2,77 +2,39 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 export const pool = new Pool({
-	host: "147.79.82.72",
-	port: 5432,
-	user: "globaluser",
-	password: "123",
-	database: "gfbdb",
+  host: "147.79.82.72",
+  port: 5432,
+  user: "globaluser",
+  password: "123",
+  database: "gfbdb",
 });
 
 async function testConnection() {
-	const client = await pool.connect();
+  const client = await pool.connect();
 
-	try {
-		console.log('Conexão bem-sucedida ao PostgreSQL!');
-
-	} catch (err) {
-		console.error('Erro ao conectar ao PostgreSQL:', err);
-	} finally {
-	 client.release();
-		console.log('Conexão liberada.');
-	}
-}
-
-const createUsers = async () => {
-  const client = await pool.connect()
-  try{
-    console.log('db conectado.')
-
-    const query = `
-    CREATE TABLE IF NOT EXISTS correntistas (
-    id SERIAL PRIMARY KEY,
-    cpf TEXT UNIQUE NOT NULL,
-    name TEXT NOT NULL,
-    email TEXT NOT NULL,
-    password TEXT NOT NULL
-    )
-    `
-
-    await client.query(query)
-    console.log('tabela users criada com sucesso')
-  } catch(error){
-    console.error('erro ao criar tabela: ', error)
-  } finally{
-    client.release()
-    console.log('conexão liberado')
+  try {
+    console.log("Conexão bem-sucedida ao PostgreSQL!");
+  } catch (err) {
+    console.error("Erro ao conectar ao PostgreSQL:", err);
+  } finally {
+    client.release();
+    console.log("Conexão liberada.");
   }
 }
 
 const createBankAccount = async () => {
-	const client = await pool.connect();
-  try{
-    console.log('db conectado')
+  const client = await pool.connect();
+  try {
+    console.log("db conectado");
 
-    const query = `
-      CREATE SEQUENCE account START 10 INCREMENT 1;
+    
 
-      CREATE TABLE IF NOT EXISTS conta_bancaria (
-      id SERIAL PRIMARY KEY,
-      user_id INT REFERENCES correntistas(id) ON DELETE CASCADE,
-      agency VARCHAR(4) NOT NULL DEFAULT '0001',
-      account_number INT UNIQUE DEFAULT nextval('account')
-      )
-    `
-		await client.query(query)
+    console.log("tabela bankAccount criada com sucesso");
+  } catch (error) {
+    console.error("erro ao criar tabela: ", error);
+  }
+};
 
-    console.log('tabela bankAccount criada com sucesso')
-  } catch(error){
-    console.error('erro ao criar tabela: ', error)
-  } 
-}
-
-createUsers()
-
-createBankAccount()
+createBankAccount();
 
 testConnection();
