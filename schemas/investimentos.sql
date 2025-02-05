c6bb171d8092fcf92a4ca0d5d4388e51be062a96
Pamela Lima <pamela.jesus.lima@gmail.com>
2025-02-05 09:47:34 -0300

    tabela investimentos atualizada

--- schemas/investimentos.sql
+++ schemas/investimentos.sql
@@ -1,14 +1,24 @@
 CREATE TABLE investimentos(
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    fk_emissor INT REFERENCES emissores(id) ON DELETE CASCADE, -- Emissor do investimento
-    codigo_emissao VARCHAR(50) NOT NULL, -- Código gerado automaticamente ou manual // receita federal - bolsa de valores - ministerio da fazenda
-    tipo_debenture VARCHAR(50) NOT NULL, -- Dropdown: Simples, Conversível, Permutável
+    fk_emissor INT REFERENCES emissores(id) ON DELETE CASCADE,
+    area_categoria VARCHAR(50) NOT NULL, -- Emissor do investimento
+    produto_tipo VARCHAR(50) NOT NULL, --produto/tipo
+    local VARCHAR(100) NOT NULL, 
     data_emissao DATE NOT NULL,
     data_vencimento DATE NOT NULL,
+    segurador VARCHAR(50) NOT NULL, 
+    re_segurador VARCHAR(50) NOT NULL,
+    numero_apolice VARCHAR(50) NOT NULL,
+    numero_subapolice VARCHAR(50) NOT NULL,
     valor_nominal_unitario NUMERIC(15, 2) NOT NULL, --valor minímo por investimento
-    quantidade_debentures INT NOT NULL,
     remuneracao VARCHAR(50) NOT NULL, -- Dropdown: Juros Fixos, IPCA, CDI, Outros
+    rentabilidade -NUMERIC(5,2) NOT NULL -- porcentagem da rentabilidade %
+    tipo_debenture VARCHAR(50) NOT NULL, -- Dropdown: Simples, Conversível, Permutável
+    quantidade_debentures INT NOT NULL,
     periodicidade_pagamento VARCHAR(50) NOT NULL, -- Dropdown: Mensal, Semestral, Anual
+    liquidez VARCHAR(50) NOT NULL,
+    serie VARCHAR(50) NOT NULL, 
+    codigo_emissao VARCHAR(50) NOT NULL, -- Código gerado automaticamente ou manual // receita federal - bolsa de valores - ministerio da fazenda
     garantias_oferecidas TEXT, --file
     finalidade_captacao TEXT, 
     amortizacao_antecipada BOOLEAN DEFAULT FALSE,
