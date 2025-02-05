2febf0f94f8bc47cfb7d1c50c3b07f2385c8fb49
Pamela Lima <pamela.jesus.lima@gmail.com>
2025-02-05 13:48:47 -0300

    painel Mercado de Tokens atualizado - puxando do banco de dados

--- backend/src/controllers/InvestmentsController.js
+++ backend/src/controllers/InvestmentsController.js
@@ -1,28 +1,29 @@
 import { pool } from "../database/connection.js";
+import { format, compareAsc } from "date-fns";
 
-export class InvestmentsController{
-    async create(req, res){
-        const db = pool
+export class InvestmentsController {
+  async create(req, res) {
+    const db = pool;
 
-        const files = req.files;
-        console.log(files)
+    const files = req.files;
+    console.log(files);
 
-        const {
-            codigo_emissao ,
-            tipo_debenture ,
-            data_emissao,
-            data_vencimento,
-            valor_nominal_unitario,
-            quantidade_debentures,
-            remuneracao,
-            periodicidade_pagamento,
-            garantias_oferecidas,
-            finalidade_captacao ,
-            amortizacao_antecipada,
-            fk_emisser 
-        } = req.body 
+    const {
+      codigo_emissao,
+      tipo_debenture,
+      data_emissao,
+      data_vencimento,
+      valor_nominal_unitario,
+      quantidade_debentures,
+      remuneracao,
+      periodicidade_pagamento,
+      garantias_oferecidas,
+      finalidade_captacao,
+      amortizacao_antecipada,
+      fk_emisser,
+    } = req.body;
 
-        const insert = `
+    const insert = `
         INSERT INTO investments (
         codigo_emissao ,
         tipo_debenture ,
@@ -43,51 +44,61 @@ export class InvestmentsController{
         fk_emisser
         )
         VALUES ( $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16,  $17)
-        `
+        `;
 
-        const values = [
-            codigo_emissao ,
-            tipo_debenture ,
-            data_emissao,
-            data_vencimento,
-            valor_nominal_unitario,
-            quantidade_debentures,
-            remuneracao,
-            periodicidade_pagamento,
-            garantias_oferecidas,
-            finalidade_captacao ,
-            amortizacao_antecipada,
-            files.riscos_mercado[0].filename ,
-            files.riscos_liquidez[0].filename ,
-            files.riscos_tecnologicos[0].filename ,
-            files.riscos_regulatorios[0].filename ,
-            files.descricao_garantias[0].filename , 
-            fk_emisser
-        ]
+    const values = [
+      codigo_emissao,
+      tipo_debenture,
+      data_emissao,
+      data_vencimento,
+      valor_nominal_unitario,
+      quantidade_debentures,
+      remuneracao,
+      periodicidade_pagamento,
+      garantias_oferecidas,
+      finalidade_captacao,
+      amortizacao_antecipada,
+      files.riscos_mercado[0].filename,
+      files.riscos_liquidez[0].filename,
+      files.riscos_tecnologicos[0].filename,
+      files.riscos_regulatorios[0].filename,
+      files.descricao_garantias[0].filename,
+      fk_emisser,
+    ];
 
-        try{
-            const result = await db.query(insert, values)
-            console.log("Investimento cadastrado com sucesso!")
-            return res.status(200).json({result})
-        }
-        catch(e){
-            return console.error(e.message)
-        }
+    try {
+      const result = await db.query(insert, values);
+      console.log("Investimento cadastrado com sucesso!");
+      return res.status(200).json({ result });
+    } catch (e) {
+      return console.error(e.message);
     }
+  }
 
-    async index(req, res){
-        const db = pool
+  async index(req, res) {
+    const db = pool;
 
-        const select = `
-        SELECT * FROM investments
-        `
+    const select = `
+        SELECT * FROM investimentos
+        `;
 
-        try{
-            const { rows } = await db.query(select)
-            return res.json( rows )
-        }
-        catch(e){
-            return res.json({"message": e.message})
-        }
+    try {
+      const { rows } = await db.query(select);
+
+      const investments = rows.map((investment) => {
+        const result = {
+          titulo: investment.local,
+          rentabilidade: `${investment.remuneracao} ${investment.rentabilidade}%`,
+          quantidade_debentures:  investment.quantidade_debentures,
+          valor_unitario : `R$ ${investment.valor_unitario}`,
+          vencimento: format(investment.data_vencimento, "MM/dd/yyyy"),
+        };
+        return result;
+      });
+
+      return res.json(investments);
+    } catch (e) {
+      return res.json({ message: e.message });
     }
+  }
 }
--- frontend/src/pages/Correntista/MercadoMoedas/index.tsx
+++ frontend/src/pages/Correntista/MercadoMoedas/index.tsx
@@ -7,11 +7,13 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { useState } from "react";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
+
+import { useState, useEffect } from "react";
+import { api } from "../../../services/api";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -33,64 +35,32 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 }));
 
-interface DataType {
-  titulo: string,
-  rentabilidadeAnual: number,
-  investimentoMinimo: number,
-  disponibilidade: number,
-  vencimento: string,
-  simule: string
+interface Investment {
+  titulo: string;
+  rentabilidade: string;
+  valor_unitario: string;
+  vencimento: string;
+  remuneracao: string;
+  quantidade_debentures: string;
+  simule: string;
 }
-
-function createData(
-  titulo: string,
-  rentabilidadeAnual: number,
-  investimentoMinimo: number,
-  disponibilidade: number,
-  vencimento: string,
-  simule: string
-) {
-  return {
-    titulo,
-    rentabilidadeAnual,
-    investimentoMinimo,
-    disponibilidade,
-    vencimento,
-    simule,
-  };
-}
-
-const rows = [
-  createData("Aracaju", 3, 10, 83214, "01/01/2027", "/"),
-  createData("Salvador", 4, 15, 313918444, "01/02/2028", "/"),
-  createData("Fortaleza", 5, 12, 780000, "01/03/2029", "/"),
-  createData("Recife", 6, 20, 185274999, "01/04/2030", "/"),
-  createData("Natal", 7, 18, 95232345, "01/05/2031", "/"),
-  createData("Maceió", 3.5, 10, 4456768, "01/06/2027", "/"),
-  createData("João Pessoa", 4.5, 14, 987655432, "01/07/2028", "/"),
-  createData("São Luís", 5.5, 16, 125672, "01/08/2029", "/"),
-  createData("Teresina", 6.2, 19, 725000000, "01/09/2030", "/"),
-  createData("Cuiabá", 7.3, 13, 591234567, "01/10/2031", "/"),
-  createData("Campo Grande", 5.7, 17, 48766321, "01/11/2027", "/"),
-  createData("Goiânia", 6.8, 15, 912345678, "01/12/2028", "/"),
-  createData("Belo Horizonte", 4.9, 10, 2158674, "01/01/2029", "/"),
-  createData("Vitória", 6.5, 18, 13876234, "01/02/2030", "/"),
-  createData("Rio de Janeiro", 5.2, 12, 823479008, "01/03/2031", "/"),
-  createData("São Paulo", 7.8, 20, 756453239, "01/04/2027", "/"),
-  createData("Curitiba", 4.1, 14, 69254780, "01/05/2028", "/"),
-  createData("Porto Alegre", 5.3, 16, 548923453, "01/06/2029", "/"),
-  createData("Florianópolis", 6.7, 13, 235567890, "01/07/2030", "/"),
-  createData("Joinville", 4.4, 10, 417896543, "01/08/2031", "/"),
-  createData("Uberlândia", 5.9, 19, 62109876, "01/09/2027", "/"),
-  createData("Campinas", 7.2, 21, 111222333, "01/10/2028", "/"),
-  createData("Ribeirão Preto", 6.3, 17, 75432109, "01/11/2029", "/"),
-  createData("Sorocaba", 4.8, 13, 65231000, "01/12/2030", "/"),
-  createData("Santos", 5.6, 15, 1398765, "01/01/2031", "/"),
-  createData("Osasco", 7.0, 14, 456789012, "01/03/2028", "/"),
-  createData("Diadema", 4.7, 16, 987654321, "01/04/2029", "/"),
-];
 
 function MercadoTabela() {
+  const [investments, setInvestments] = useState<Investment[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=> {
+    api.get('/investments')
+    .then(response => {
+      setInvestments(response.data)
+      setLoading(false)
+    })
+    .catch(error => {
+      console.error(Error)
+      setLoading(false)
+    })
+  }, [])
+
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 700 }} aria-label="customized table">
@@ -105,23 +75,23 @@ function MercadoTabela() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <StyledTableRow key={row.titulo}>
+          {investments.map((investment) => (
+            <StyledTableRow key={investment.titulo}>
               <StyledTableCell component="th" scope="row">
-                {row.titulo}
+                {investment.titulo}
               </StyledTableCell>
               <StyledTableCell align="right">
-                Selic + IPCA + {row.rentabilidadeAnual}%
+                {investment.rentabilidade}
               </StyledTableCell>
               <StyledTableCell align="right">
-                R$ {row.investimentoMinimo}
+                {investment.valor_unitario}
               </StyledTableCell>
               <StyledTableCell align="right">
-                {row.disponibilidade} cotas
+                {investment.quantidade_debentures}
               </StyledTableCell>
-              <StyledTableCell align="right">{row.vencimento}</StyledTableCell>
+              <StyledTableCell align="right">{investment.vencimento}</StyledTableCell>
               <StyledTableCell align="right">
-                <NavButton to={row.simule}>Simule</NavButton>
+                <NavButton to={investment.simule}>Simule</NavButton>
               </StyledTableCell>
             </StyledTableRow>
           ))}
@@ -131,41 +101,57 @@ function MercadoTabela() {
   );
 }
 
-function MercadoCard(){
-  function Card ({cardInfo}:{cardInfo: DataType}) {
-    return(
-      <div style={{backgroundColor: "black", padding: 15, borderRadius: 15, width: 320}}>
+function MercadoCard() {
+  const [investments, setInvestments] = useState<Investment[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=> {
+    api.get('/investments')
+    .then(response => {
+      setInvestments(response.data)
+      setLoading(false)
+    })
+    .catch(error => {
+      console.error(Error)
+      setLoading(false)
+    })
+  }, []) 
+
+
+  function Card({ cardInfo }: { cardInfo: Investment }) {
+    return (
+      <div style={{ backgroundColor: "black", padding: 15, borderRadius: 15, width: 320 }}>
         <h1>{cardInfo.titulo}</h1>
-      <table>
-        <tbody>
-          <th><strong>Rentabilidade anual:</strong></th>
-          <td>Selic + IPCA + {cardInfo.rentabilidadeAnual}%</td>
-        </tbody>
-        <tbody>
-          <th><strong>Valor da Cota:</strong></th>
-          <td>R$ {cardInfo.investimentoMinimo}</td>
-        </tbody>
-        <tbody>
-          <th><strong>Disponibilidade:</strong></th>
-          <td>{cardInfo.disponibilidade} cotas</td>
-        </tbody>
-        <tbody>
-          <th><strong>Vencimento:</strong></th>
-          <td>{cardInfo.vencimento}</td>
-        </tbody>
-      </table>
-      <NavButton to={cardInfo.simule}>Simule</NavButton>
+        <table>
+          <tbody>
+            <th><strong>Rentabilidade anual:</strong></th>
+            <td>{cardInfo.rentabilidade}</td>
+          </tbody>
+          <tbody>
+            <th><strong>Valor Unitário:</strong></th>
+            <td>{cardInfo.valor_unitario}</td>
+          </tbody>
+          <tbody>
+            <th><strong>Disponibilidade:</strong></th>
+            <td>{cardInfo.quantidade_debentures}</td>
+          </tbody>
+          <tbody>
+            <th><strong>Vencimento:</strong></th>
+            <td>{cardInfo.vencimento}</td>
+          </tbody>
+        </table>
+        <NavButton to={cardInfo.simule}>Simule</NavButton>
 
       </div>
     )
   }
- return(
-  <div style={{display: "flex", gap: 25, flexWrap: "wrap", maxWidth: 1200, justifyContent: "center"}}>
-    {rows.map((row)=>(
-    <Card cardInfo={row}/>
-    ))}
-  </div>
- )
+  return (
+    <div style={{ display: "flex", gap: 25, flexWrap: "wrap", maxWidth: 1200, justifyContent: "center" }}>
+      {investments.map((investment) => (
+        <Card cardInfo={investment} />
+      ))}
+    </div>
+  )
 }
 
 const MercadoMoedas = () => {
@@ -182,7 +168,7 @@ const MercadoMoedas = () => {
           row
           aria-labelledby="demo-row-radio-buttons-group-label"
           name="row-radio-buttons-group"
-          style={{justifyContent: "center"}}
+          style={{ justifyContent: "center" }}
           value={viewMode}
           onChange={handleRadioChange}
         >
@@ -208,13 +194,13 @@ const MercadoMoedas = () => {
       case "table":
         return <MercadoTabela />;
       case "card":
-        return <MercadoCard/>
+        return <MercadoCard />
     }
   }
 
   return (
     <NavBody>
-      <h1 style={{marginTop: 50, textAlign: "center"}}>Mercado de Tokens</h1>
+      <h1 style={{ marginTop: 50, textAlign: "center" }}>Mercado de Tokens</h1>
       <Container>
         <RowRadioButtonsGroup />
         <SwtichBody />
